fde6d77df89a7132f27e7e35bfb35364d256fd9a
Fadhli <marvelouslyfadhli@gmail.com>
2023-11-19 02:47:43 +0800

    remove time bar from barchart

--- src/components/BarCharts.jsx
+++ src/components/BarCharts.jsx
@@ -22,7 +22,6 @@ export default function BarCharts(props) {
                     <YAxis dataKey="relativehumidity_2m" width={20}/>
                     <Tooltip />
                     <Legend />
-                    <Bar name="Time (Hourly)" dataKey="time" fill="#8884d8" />
                     <Bar name="Relative Humidity (2m)" dataKey="relativehumidity_2m" fill="#82ca9d" />
                     <Brush startIndex={0} endIndex={23} height={20}></Brush>
                 </BarChart>
